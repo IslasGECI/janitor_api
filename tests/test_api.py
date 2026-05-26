8b38b3e687cb1b79c508839b5a5dd4ed00d932dd
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-05-26 18:53:26 +0000

    🛑🧪 Fail tests

---
+++ tests/test_api.py
@@ -0,0 +1,39 @@
+from janitor_api.api import api
+
+import io
+from fastapi.testclient import TestClient
+
+client = TestClient(api)
+
+
+def test_api_check_traps_ids():
+    traps_positions_path = "tests/data/IG_POSICION_TRAMPAS_24MAY2026.xlsx"
+    mapsource_path = "tests/data/IG_MAPSOURCE_TRAMPA_24MAY2026.txt"
+
+    with open(traps_positions_path, "rb") as f:
+        file_like_traps_positions = io.BytesIO(f.read())
+
+    with open(mapsource_path, "rb") as f:
+        file_like_mapsource = io.BytesIO(f.read())
+
+    remote_traps_positions_path = "IG_POSICION_TRAMPAS_24MAY2026.xlsx"
+    remote_mapsource_path = "IG_MAPSOURCE_TRAMPA_24MAY2026.txt"
+
+    request = {
+        "url": "/check_traps_ids",
+        "files": {
+            "data_path": (
+                remote_traps_positions_path,
+                file_like_traps_positions,
+                "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+            ),
+            "initial_parameters_path": (
+                remote_mapsource_path,
+                file_like_mapsource,
+                "text/tab-separated-values",
+            ),
+        },
+    }
+
+    response = client.post(**request)
+    assert response.status_code == 200

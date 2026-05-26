42e79a660387ec0b0112708cddc2b3a67795661c
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-05-26 20:43:43 +0000

    ♻️  Refactor

--- tests/test_api.py
+++ tests/test_api.py
@@ -16,19 +16,16 @@ def test_api_check_traps_ids():
     with open(mapsource_path, "rb") as f:
         file_like_mapsource = io.BytesIO(f.read())
 
-    remote_traps_positions_path = "IG_POSICION_TRAMPAS_24MAY2026.xlsx"
-    remote_mapsource_path = "IG_MAPSOURCE_TRAMPA_24MAY2026.txt"
-
     request = {
         "url": "/check_traps_ids",
         "files": {
             "data_path": (
-                remote_traps_positions_path,
+                traps_positions_path,
                 file_like_traps_positions,
                 "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
             ),
             "initial_parameters_path": (
-                remote_mapsource_path,
+                mapsource_path,
                 file_like_mapsource,
                 "text/tab-separated-values",
             ),
@@ -36,7 +33,6 @@ def test_api_check_traps_ids():
     }
 
     response = client.post(**request)
-    print(response.json())
     assert response.status_code == 200
     assert "💚 La revisión de trampas es correcta 💚" == response.json()["message"]
 
@@ -44,25 +40,19 @@ def test_api_check_traps_ids():
         "tests/data/IG_MAPSOURCE_TRAMPA_24MAY2026_with_missing_traps.txt"
     )
 
-    with open(traps_positions_path, "rb") as f:
-        file_like_traps_positions = io.BytesIO(f.read())
-
     with open(mapsource_path_with_missing_trap, "rb") as f:
         file_like_mapsource = io.BytesIO(f.read())
-
-    remote_traps_positions_path = "IG_POSICION_TRAMPAS_24MAY2026.xlsx"
-    remote_mapsource_path = "IG_MAPSOURCE_TRAMPA_24MAY2026.txt"
 
     request = {
         "url": "/check_traps_ids",
         "files": {
             "data_path": (
-                remote_traps_positions_path,
+                traps_positions_path,
                 file_like_traps_positions,
                 "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
             ),
             "initial_parameters_path": (
-                remote_mapsource_path,
+                mapsource_path_with_missing_trap,
                 file_like_mapsource,
                 "text/tab-separated-values",
             ),
@@ -70,6 +60,5 @@ def test_api_check_traps_ids():
     }
 
     response = client.post(**request)
-    print(response.json())
     assert response.status_code == 200
     assert "TC-01-056-JA" in response.json()["message"]

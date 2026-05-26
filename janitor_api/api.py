e799e8c18171cf5a879d2808e83d22dd9ef98560
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-05-26 18:57:14 +0000

    ✅ Pass tests

--- janitor_api/api.py
+++ janitor_api/api.py
@@ -1,2 +1,8 @@
-def add_offset(augend: int, addend: int) -> int:
-    return augend + addend
+from fastapi import FastAPI, UploadFile
+
+api = FastAPI()
+
+
+@api.post("/check_traps_ids")
+async def check_traps_ids(data_path: UploadFile, initial_parameters_path: UploadFile):
+    return {"status": "ok"}

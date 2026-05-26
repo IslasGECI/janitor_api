053aaec62e25cdd6052d5052d46fddddfe615e6e
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-05-26 20:48:51 +0000

    ♻️  Refactor

--- janitor_api/api.py
+++ janitor_api/api.py
@@ -9,7 +9,6 @@ api = FastAPI()
 async def check_traps_ids(positions_path: UploadFile, mapsource_path: UploadFile):
     await write_internal_file(positions_path)
     await write_internal_file(mapsource_path)
-
     data_path_filename = positions_path.filename
     mapsource_path_filename = mapsource_path.filename
 
@@ -19,12 +18,18 @@ async def check_traps_ids(positions_path: UploadFile, mapsource_path: UploadFile
         f"readMS::check_traps('{data_path_filename}', '{mapsource_path_filename}')",
     ]
     result = subprocess.run(command, capture_output=True, text=True)
+
+    result_message = format_check_traps_ids_message(result)
+    return JSONResponse(content={"message": result_message})
+
+
+def format_check_traps_ids_message(result):
     result_stderr_splitted = result.stderr.splitlines()
     if result.returncode == 0:
         result_message = result_stderr_splitted[0]
     else:
         result_message = result_stderr_splitted[1]
-    return JSONResponse(content={"message": result_message})
+    return result_message
 
 
 async def write_internal_file(uploaded_data_path):
